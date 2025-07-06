15de9b720ea1fa78528b204fdcf9cbaca9995d52
yareach8345 <mine8345@naver.com>
2025-07-06 19:12:32 +0900

    feature - mqtt를 위한 env파일 설정

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,21 +1,30 @@
 import { Injectable, Logger } from '@nestjs/common'
 import mqtt, { MqttClient } from 'mqtt'
 import { NotifyBaseService } from '../commons/base/notify-base.service'
+import { requireEnv } from '../commons/utils/env.util'
 
 @Injectable()
 export class MqttService extends NotifyBaseService {
   private readonly mqttClient: MqttClient
+  private readonly baseTopic: string
   private logger: Logger = new Logger(MqttService.name)
 
   constructor() {
     super()
-    this.mqttClient = mqtt.connect('mqtt://localhost')
+
+    const baseTopic = process.env.MQTT_BASE_TOPIC
+    this.baseTopic = baseTopic !== undefined ? baseTopic + '/' : ""
+
+    this.mqttClient = mqtt.connect(requireEnv('MQTT_URL'))
     this.mqttClient.on('connect', () => {
       this.logger.log('MQTT 연결 성공')
     })
   }
 
   notify(topic: string, payload: string) {
-    this.mqttClient.publish(topic, payload)
+    this.mqttClient.publish(
+      `${this.baseTopic}${topic}`,
+      payload
+    )
   }
 }
